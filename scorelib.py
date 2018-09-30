dda2aec9e65a79837ed50932d969a91f622ea7ae
DariaSereb <chyna93@Gmail.com>
2018-09-30 15:40:22 +0200

    Create scorelib.py

---
+++ scorelib.py
@@ -0,0 +1,55 @@
+class Print:
+    def __init__(self,edition,print_id,pariture):
+       self.edition = edition
+       self.print_id = print_id
+       self.pariture = pariture
+     
+          
+    def format(self):
+        print("Print Number: {}".format(self.print_id), )
+         
+    
+    def composition(self):
+         return self.edition.composition
+        
+class Edition:
+    
+     edition = []
+     
+    def __init__(self,composition,authors,name):
+        self.composition = composition
+        self.authors = authors
+        self.name = name
+
+        def authors_name(self, name):
+        self.authors.append(Person(name))
+
+class Composition:
+    def __init__(self, name, incipit, key, genre, year, composer):
+        self.name = name
+        self.incipit = incipit
+        self.key = key
+        self.genre = genre
+        self.year = year
+        self.voices = []
+        self.authors = []
+
+    def add_voice(self, voice_range, name):
+        self.voices.append(Voice(voice_range, name))
+
+    def add_author(self, name):
+        self.authors.append(Person(name))
+
+class Voice
+   def __init__(self,name,range):
+       self.name = name
+       self.range = range
+
+class Person
+   def __init__(self,name,born,died)
+        self.name = name
+        self.born = born
+        self.died = died
+
+def main()
+        print(argv)

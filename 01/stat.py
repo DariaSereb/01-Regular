a01af8adb1853801fd09ac25bd02cfba6ab8a4a0
DariaSereb <chyna93@Gmail.com>
2018-09-30 13:02:24 +0200

    Update stat.py

--- 01/stat.py
+++ 01/stat.py
@@ -8,24 +8,16 @@ if sys.argv[0] == "composer":
     document_text = open('scorelib.txt',encoding="utf8")
     text_string = document_text.read()
 
-    match_pattern = re.findall(r'(?<=Composer: )(\S*..\S*..[a-z]+|\S*..\S*|\b)', text_string)
-    match_pattern2 = re.findall(r'(?<=; )([a-z]*\w{4,15}..[a-z]*..[a-z]*..[a-z]+|[a-z]*\w{4,15}..[a-z]*..[a-z]+)', text_string)
-    match_pattern2. remove('Courante nova; Courante')
-    match_pattern2. remove('Rigadoon; Iers lied')
-    match_pattern2. remove('Saltarello ')
-    match_pattern2. remove('Molto Allegro')
-    match_pattern. remove('Petzold, Chr; Anonymous')
-    match_pattern.append('Petzold')
+   
 
-   for word in match_pattern:
-    count = frequency.get(word,0)
-    frequency[word] = count + 1
-
-   for word in match_pattern2:
-    count = frequency.get(word,0)
-    frequency[word] = count + 1
-     
-   frequency_list = frequency.keys()
+    for  lines in re.findall(r'(?<=Composer: )(\S*..\S*..\S*..\S*..\S*..\S*..\S*..\S*..\S*..\S*..\S+|\S*..\S*|\b)', text_string):
+          match_pattern = re.split(r'; ', lines)
+    
+    
+          for word in match_pattern:
+               count = frequency.get(word,0)
+               frequency[word] = count + 1
+          frequency_list = frequency.keys() 
     
    for words in frequency_list:
     

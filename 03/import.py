62eca4cbf91875b4a19ee3edad8044780d3889ef
DariaSereb <chyna93@Gmail.com>
2018-10-16 22:59:40 +0200

    Update import.py

--- 03/import.py
+++ 03/import.py
@@ -1,4 +1,3 @@
-
 import sys
 import sqlite3
 import scorelib as module
@@ -13,34 +12,8 @@ def initDb(datFile, schemaFile):
 
     return db
 
-def insertData(prints,db):
-    cursor = db.cursor()
-    composerIds = []
-    editorIds = []
 
-    for p in prints:
-        for composer in p.composition().authors:
-            composerIds.append(insertPerson(composer,cursor))
-        for editor in p.edition.authors:
-            editorsIds.append(insertPerson(editor, cursor))
-
-        scoreId = insertScore(p.composition(), cursor)
-        for i, voice in enumerate(p.composition().voices):
-            insertVoice(voice, scoreId, i+1, cursor)
-        for composerId in composerIds:
-            insertScoreAuthor(scoreId, composerId, cursor)
-
-        editionId = insertEdition(p.edition, scoreId, cursor)
-        for editorId in editorIds:
-            insertEditionAuthor(editionId, editorId, cursor)
-
-        insertPrint(p, editionId, cursor)
-        composerIds = []
-        editorIds = []
-
-
-
-def insertScore(composition, cursor):
+def insert_Score(composition, cursor):
     cursor.execute("SELECT * FROM score WHERE name IS ? AND genre IS ? AND key IS ? AND incipit IS ? AND year IS ?", 
                     (composition.name, composition.genre, composition.key, composition.incipit, composition.year,))
     match = cursor.fetchone()
@@ -52,7 +25,7 @@ def insertScore(composition, cursor):
     else:
         return match[0]
     
-def insertPerson(person, cursor):
+def Person_insrt(person, cursor):
     cursor.execute("SELECT * FROM person WHERE name IS ?", (person.name,))
     match = cursor.fetchone()
 
@@ -71,7 +44,7 @@ def insertPerson(person, cursor):
         cursor.execute("UPDATE person SET born=?, died=? WHERE name=?", (born, died, person.name))
         return match[0]
     
-def insertEdition(edition, scoreId, cursor):
+def Edition_insrt(edition, scoreId, cursor):
     cursor.execute("SELECT * FROM edition WHERE score IS ? AND name IS ?", (scoreId, edition.name,))
     match = cursor.fetchone()
 
@@ -81,7 +54,7 @@ def insertEdition(edition, scoreId, cursor):
     else:
         return match[0]
 
-def insertVoice(voice, scoreId, number, cursor):
+def Voice_insrt(voice, scoreId, number, cursor):
     cursor.execute("SELECT * FROM voice WHERE number IS ? AND score IS ? AND range IS ? AND name IS ?",
                     (number, scoreId, (voice.range if voice is not None else None), (voice.name if voice is not None else None)))
     match = cursor.fetchone()
@@ -90,31 +63,61 @@ def insertVoice(voice, scoreId, number, cursor):
         cursor.execute("INSERT INTO voice(number, score, range, name) VALUES(?, ?, ?, ?)", 
                         (number, scoreId, (voice.range if voice is not None else None), (voice.name if voice is not None else None)))
 
-def insertEditionAuthor(editionId, editorId, cursor):
+def Edition_insrt(editionId, editorId, cursor):
     cursor.execute("SELECT * FROM edition_author WHERE edition IS ? AND editor IS ?", (editionId, editorId,))
     match = cursor.fetchone()
 
     if match is None:
         cursor.execute("INSERT INTO edition_author(edition, editor) VALUES(?, ?)", (editionId, editorId))
 
-def insertScoreAuthor(scoreId, composerId, cursor):
+def Author_insrt(scoreId, composerId, cursor):
     cursor.execute("SELECT * FROM score_author WHERE score IS ? AND composer IS ?", (scoreId, composerId,))
     match = cursor.fetchone()
 
     if match is None:
         cursor.execute("INSERT INTO score_author(score, composer) VALUES(?, ?)", (scoreId, composerId))
 
-def insertPrint(print, editionId, cursor):
+def PrintID_insrt(print, editionId, cursor):
     cursor.execute("SELECT * FROM print WHERE id IS ? AND partiture IS ? AND edition IS ?", (print.print_id, print.partiture, editionId))
     match = cursor.fetchone()
 
     if match is None:
         cursor.execute("INSERT INTO print(id, partiture, edition) VALUES(?, ?, ?)", (print.print_id, print.partiture, editionId))
 
+
+        
+def Data_insrt(prints,db):
+    cursor = db.cursor()
+    composerIds = []
+    editorIds = []
+
+    for p in prints:
+        for composer in p.composition().authors:
+            composerIds.append(Person_instr(composer,cursor))
+        for editor in p.edition.authors:
+            editorsIds.append(Person_instr(editor, cursor))
+
+        scoreId = insertScore(p.composition(), cursor)
+        
+        for i, voice in enumerate(p.composition().voices):
+            Voice_instr(voice, scoreId, i+1, cursor)
+        for composerId in composerIds:
+            Score_instr(scoreId, composerId, cursor)
+
+        editionId = Edition_instr(p.edition, scoreId, cursor)
+        
+        for editorId in editorIds:
+            Author_instr(editionId, editorId, cursor)
+
+        PrintID_instr(p, editionId, cursor)
+        composerIds = []
+        editorIds = []
+
+        
 def main():
     
     db = initDb(sys.argv[2], "scorelib.sql")
-    insertData(module.load(sys.argv[1]), db)
+    Data_instr(module.load(sys.argv[1]), db)
     db.commit()
     db.close()
 
